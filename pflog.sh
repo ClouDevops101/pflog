3b2c18315aba7362c444da112d0f74066186c653
LinuxArchitects <heddar.abdelilah@live.fr>
2016-01-20 15:56:24 +0100

    bonjour

--- pflog.sh
+++ pflog.sh
@@ -13,6 +13,8 @@ echo '!your_public_ip' >> /etc/pf-files/FR_blocked_zone
 curl http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt > /etc/pf-files/Emerging-Block-IP
 
 
+
+
 pfctl -ef /etc/pf.conf
 ifconfig pflog0 create
 /usr/sbin/tcpdump -lnettti pflog0 | /usr/bin/logger -t pf -p local2.info
